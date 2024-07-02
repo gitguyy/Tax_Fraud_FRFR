4ea521e91749afe9b2fa7169f79109643b2f44ea
Erihs <115496133+Erihs@users.noreply.github.com>
2024-07-02 19:03:58 +0200

    Footsteps

--- Tax_Fraud_demons/Assets/Script/Player/PlayerController.cs
+++ Tax_Fraud_demons/Assets/Script/Player/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerController : CharacterController
 {
+    public AudioClip[] footstepSounds; // Array to hold footstep sounds
+    private AudioSource audioSource;
     
       protected override void Start()
     {
         base.Start();
+        
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>(); // Add an AudioSource if not present
+        }
     }
 
     // Update is called once per frame
@@ -21,5 +29,16 @@ public class PlayerController : CharacterController
     void getInput()
     {
         direction = Input.GetAxisRaw("Horizontal");
+    }
+    
+    // Method to play a random footstep sound
+    public void PlayFootstep()
+    {
+        if (footstepSounds.Length > 0)
+        {
+            int randomIndex = Random.Range(0, footstepSounds.Length);
+            audioSource.clip = footstepSounds[randomIndex];
+            audioSource.Play();
+        }
     }
 }
