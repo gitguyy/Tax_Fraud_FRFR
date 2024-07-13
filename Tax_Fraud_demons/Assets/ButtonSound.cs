d85e4298b7a50dc631364dfed881aac901662bba
gitguyy <gilmoreschneider34@gmail.com>
2024-07-13 14:22:48 +0200

    button sounds

---
+++ Tax_Fraud_demons/Assets/ButtonSound.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    Button button;
+    [SerializeField]
+    SoundManager soundManager;
+    
+    void OnEnable()
+    {
+        soundManager = FindAnyObjectByType<SoundManager>();
+        button = GetComponent<Button>();
+        button.onClick.RemoveListener(soundManager.PlayButtonSound);
+        button.onClick.AddListener(soundManager.PlayButtonSound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Start()
+    {
+        
+    }
+}
